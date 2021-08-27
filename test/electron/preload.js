f1e412c1a22f2b505f88028e3c9a145a57fde22b
Nicholas Sterling <Nicholas.Sterling@gmail.com>
2021-08-27 02:57:56 -0500

    Not sure

--- test/electron/preload.js
+++ test/electron/preload.js
@@ -1,5 +1,7 @@
 const native = require('./index.node');
 
+window.neonc = native;
+
 window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('greeting').innerText = native.hello();
 })
@@ -7,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
 window.crash = function() {
   try {
     console.log("Calling native.crash()");
-    native.crash();
+    native.crash_deep();
   } catch (e) {
     console.log("It crashed:\n", e, "\n\nStack:\n", e.stack);
   }
